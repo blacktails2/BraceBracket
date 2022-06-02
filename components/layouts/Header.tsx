b48e747181c5b5e4fadfc19b8b8599eb147e344a
tohutohu <tohu.soy@gmail.com>
2022-06-02 20:05:36 +0900

    レスポンシブ対応

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -11,48 +11,83 @@ export const Header: FC = () => {
     <header className={styles.header}>
       <div>
         <nav className={styles.navbar}>
-          <div className={styles.navbarRight}>
+          <div className={styles.navbarLeft}>
             <Link href="/">
-              <a>
-                <Image
-                  src="/image/logotype.svg"
-                  width={292}
-                  height={30}
-                  alt="ロゴ"
-                />
-              </a>
+              <a className={styles.topLink}></a>
             </Link>
           </div>
-          <div className={styles.navbarLeft}>
+          <div className={styles.navbarRight}>
             <div className={styles.headerLinkContainer}>
               <Link href="/" passHref>
                 <a className={styles.headerLink}>Top</a>
               </Link>
+              <Link href="/" passHref>
+                <a className={styles.headerLink}>How to Use</a>
+              </Link>
+              <a
+                href="https://github.com/blacktails2/BraceBracket"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.headerLink}
+              >
+                GitHub
+              </a>
+              <a
+                href="https://discord.gg/TgMFQEqxY9"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.headerLink}
+              >
+                Community
+              </a>
             </div>
-            <Link href="/" passHref>
-              <a className={styles.headerLink}>How to Use</a>
-            </Link>
-            <a
-              href="https://github.com/blacktails2/BraceBracket"
-              target="_blank"
-              rel="noreferrer"
-              className={styles.headerLink}
-            >
-              GitHub
-            </a>
-            <a
-              href="https://discord.gg/TgMFQEqxY9"
-              target="_blank"
-              rel="noreferrer"
-              className={styles.headerLink}
-            >
-              Community
-            </a>
             <Link href={`/create${id ? `?id=${id}` : ""}`}>
               <a className={styles.createLink}>
                 {id ? "スコアボードを修正" : "スコアボードを作成"}
               </a>
             </Link>
+            <div className={styles.hamburgerMenu}>
+              <input
+                type="checkbox"
+                className={styles.dummyButton}
+                id="hamburgerMenu"
+              />
+              <label htmlFor="hamburgerMenu" className={styles.menuButton}>
+                <span></span>
+              </label>
+              <div className={styles.menuContent}>
+                <ul>
+                  <li>
+                    <Link href="/" passHref>
+                      <a>Top</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/" passHref>
+                      <a>How to Use</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <a
+                      href="https://github.com/blacktails2/BraceBracket"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      GitHub
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://discord.gg/TgMFQEqxY9"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Community
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </nav>
       </div>
