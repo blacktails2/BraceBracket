4d609a18a808d4e224a6b06c6474df4950b8222d
tohutohu <tohu.soy@gmail.com>
2022-08-14 21:53:05 +0900

    一つのブラケットのサイズが大きかった時に判定が正しくできなかった問題を修正

--- libs/getEventInfo.ts
+++ libs/getEventInfo.ts
@@ -10,8 +10,58 @@ export type EventInfo = {
   eventName: string
 }
 
+interface QueryResponse {
+  data?: Data
+  errors?: string[]
+}
+
+interface Data {
+  tournament?: Tournament
+}
+
+interface Tournament {
+  id?: number
+  name?: string
+  events?: Event[]
+}
+
+interface Event {
+  id?: number
+  name?: string
+  slug?: string
+  phases?: Phase[]
+}
+
+interface Phase {
+  id?: number
+  phaseGroups?: PhaseGroups
+}
+
+interface PhaseGroups {
+  nodes?: Node[]
+}
+
+interface Node {
+  id?: number
+  sets?: Sets
+}
+
+interface Sets {
+  pageInfo?: PageInfo
+  nodes?: Node2[]
+}
+
+interface PageInfo {
+  totalPages?: number
+}
+
+interface Node2 {
+  wPlacement?: number
+  lPlacement?: number
+}
+
 const query = `
-query Events($tourneySlug: String!, $phaseId: ID!, $phaseGroupId: ID!) {
+query Events($tourneySlug: String!, $phaseId: ID!, $phaseGroupId: ID!, $setsPage: Int) {
   tournament(slug: $tourneySlug) {
     id
     name
@@ -24,7 +74,10 @@ query Events($tourneySlug: String!, $phaseId: ID!, $phaseGroupId: ID!) {
         phaseGroups (query: {filter: {id: [$phaseGroupId]}}){
           nodes {
             id
-            sets {
+            sets(page: $setsPage, sortType: ROUND, perPage: 50) {
+              pageInfo {
+                totalPages
+              }
               nodes {
                 wPlacement
                 lPlacement
@@ -36,6 +89,27 @@ query Events($tourneySlug: String!, $phaseId: ID!, $phaseGroupId: ID!) {
     }
   }
 }`
+
+const fetchEventsInfo = async (variables: {
+  tourneySlug: string
+  phaseId: string
+  phaseGroupId: string
+  setsPage: number
+}): Promise<QueryResponse> => {
+  return await fetch(`https://api.smash.gg/gql/alpha`, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer 4c55422a25fb010184f6eb3612292f01",
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      encoding: "utf-8",
+    },
+    body: JSON.stringify({
+      query: query,
+      variables,
+    }),
+  }).then((res) => res.json())
+}
 
 export const getEventInfo = async (
   url?: string
@@ -59,49 +133,42 @@ export const getEventInfo = async (
     tourneySlug: tournarySlug,
     phaseId: phaseId,
     phaseGroupId: phaseGroupId,
+    setsPage: 1,
   }
-
-  const res = await fetch(`https://api.smash.gg/gql/alpha`, {
-    method: "POST",
-    headers: {
-      Authorization: "Bearer 4c55422a25fb010184f6eb3612292f01",
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      encoding: "utf-8",
-    },
-    body: JSON.stringify({
-      query: query,
-      variables,
-    }),
-  }).then((res) => res.json())
+  let res = await fetchEventsInfo(variables)
 
   if (res.errors) {
     console.error(res.errors)
     return "大会情報の取得中にエラーが発生しました。"
   }
+
+  const totalPages =
+    res?.data?.tournament?.events
+      ?.find((e) => e.slug === eventSlug)
+      ?.phases?.find((p) => p.id?.toString() === phaseId)
+      ?.phaseGroups?.nodes?.find((p) => p.id?.toString() === phaseGroupId)?.sets
+      ?.pageInfo?.totalPages ?? 1
+
+  if (totalPages !== 1) {
+    variables.setsPage = totalPages
+    res = await fetchEventsInfo(variables)
+    if (res.errors) {
+      console.error(res.errors)
+      return "大会情報の取得中にエラーが発生しました。"
+    }
+  }
+
   console.log(res)
   try {
     const tournamentName = res?.data?.tournament?.name
     const eventName = res?.data?.tournament?.events?.find(
-      (e: { name: string; slug: string }) => e.slug === eventSlug
+      (e) => e.slug === eventSlug
     )?.name
     const isTop8Bracket = res?.data?.tournament?.events
-      ?.find((e: { name: string; slug: string }) => e.slug === eventSlug)
-      ?.phases?.find(
-        (p: {
-          id: string
-          phaseGroups?: {
-            nodes?: [
-              { id: string; sets?: { nodes?: [{ wPlacement: number }] } }
-            ]
-          }
-        }) => p.id.toString() === phaseId
-      )
-      ?.phaseGroups?.nodes?.find(
-        (p: { id: string; sets?: { nodes?: [{ wPlacement: number }] } }) =>
-          p.id.toString() === phaseGroupId
-      )
-      ?.sets?.nodes?.find((s: { wPlacement: number }) => s.wPlacement === 1)
+      ?.find((e) => e.slug === eventSlug)
+      ?.phases?.find((p) => p.id?.toString() === phaseId)
+      ?.phaseGroups?.nodes?.find((p) => p.id?.toString() === phaseGroupId)
+      ?.sets?.nodes?.find((s) => s.wPlacement === 1)
 
     if (!tournamentName || !eventName) {
       return "大会名・イベント名の取得に失敗しました。"
