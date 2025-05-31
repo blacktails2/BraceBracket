a57b2d7fc38e0f456f6f0676d8da52008a2b8f1e
tohutohu <tohu.soy@gmail.com>
2025-06-01 07:08:17 +0900

    chore: Start.gg APIトークンを更新

--- libs/getEventInfo.ts
+++ libs/getEventInfo.ts
@@ -99,7 +99,7 @@ const fetchEventsInfo = async (variables: {
   return await fetch(`https://api.smash.gg/gql/alpha`, {
     method: "POST",
     headers: {
-      Authorization: "Bearer 4c55422a25fb010184f6eb3612292f01",
+      Authorization: "Bearer 2d1a68f32b1baf8b2b25aae5569f9dca",
       "Content-Type": "application/json",
       Accept: "application/json",
       encoding: "utf-8",
