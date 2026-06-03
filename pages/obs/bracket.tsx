afbabe21078b9d2a04ab2735f3fded70a1dd9111
blacktails2 <blacktails2@gmail.com>
2026-06-04 01:05:23 +0200

    fix: update startgg api key

--- pages/obs/bracket.tsx
+++ pages/obs/bracket.tsx
@@ -88,7 +88,7 @@ const loadTop8Bracket = async (phaseGroupId: string) => {
   const res = (await fetch("https://api.smash.gg/gql/alpha", {
     method: "POST",
     headers: {
-      Authorization: "Bearer 2d1a68f32b1baf8b2b25aae5569f9dca",
+      Authorization: "Bearer a46003b90d4efcb422cccf319adaed49",
       "content-Type": "application/json",
       Accept: "application/json",
       encoding: "utf-8",
